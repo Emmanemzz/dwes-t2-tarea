cb1a1337179bf245b74fbe03b844da9080fe7457
Emmanemzz <emmarondinelli.ts@gmail.com>
2025-10-23 11:43:49 +0200

    index

---
+++ tarea2/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menú</title>
+</head>
+<body>
+    <nav>
+        <h1>Menú de opciones</h1>
+        <ul>
+            <li><a href="./biblioteca.php">Biblioteca</a></li>
+            <li><a href="./academy.php">Academy</a></li>
+            <li><a href="./triangle.php">Triangle</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
